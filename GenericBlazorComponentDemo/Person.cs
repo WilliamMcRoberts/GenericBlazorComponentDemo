b725cd4aca2f01b8ab93da7bbdc1ffd6550c8527
WilliamMcRoberts <wamcroberts2@gmail.com>
2022-12-23 21:09:55 -0500

    Add project files.

---
+++ GenericBlazorComponentDemo/Person.cs
@@ -0,0 +1,10 @@
+﻿namespace GenericBlazorComponentDemo;
+
+public class Person
+{
+    public string Name { get; set; }
+
+    public string Email { get; set; }
+
+    public int Age { get; set; }
+}
